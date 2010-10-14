8918c76b3ec366c1e1a050c5f3f10f2be56c3b5c
Shane Tomlinson <set117@yahoo.com>
2010-10-14 22:26:03 +0000

    doing the class rename, documenting what collection is for

--- src/listPluginBindToCollection.js
+++ src/listPluginBindToCollection.js
@@ -1,21 +1,26 @@
 /**
  * A plugin that binds a list to a collection.  Listens for onInsert and onRemove
  * events, when this happens, the list is automatically updated
- * @class AFrame.ListPluginBindToMVCArray
+ * @class AFrame.ListPluginBindToCollection
  * @extends AFrame.Plugin
  * @constructor
  */
-AFrame.ListPluginBindToMVCArray = function() {
-	AFrame.ListPluginBindToMVCArray.superclass.constructor.apply( this, arguments );
+AFrame.ListPluginBindToCollection = function() {
+	AFrame.ListPluginBindToCollection.superclass.constructor.apply( this, arguments );
 };
 
-AFrame.extend( AFrame.ListPluginBindToMVCArray, AFrame.Plugin, {
+AFrame.extend( AFrame.ListPluginBindToCollection, AFrame.Plugin, {
 	init: function( config ) {
+		/**
+		 * The collection to bind to
+		 * @config collection
+		 * @type {collection}
+		 */
 		this.collection = config.collection;
 		this.collection.bindEvent( 'onInsert', this.onInsert, this );
 		this.collection.bindEvent( 'onRemove', this.onRemove, this );
 		
-		AFrame.ListPluginBindToMVCArray.superclass.init.apply( this, arguments );
+		AFrame.ListPluginBindToCollection.superclass.init.apply( this, arguments );
 	},
 	
 	onInsert: function( index, item ) {
