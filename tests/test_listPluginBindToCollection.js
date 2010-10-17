259c15fb87aa3e292ce6dc6fcf840acda23e0ed3
Shane Tomlinson <set117@yahoo.com>
2010-10-17 13:58:01 +0000

    Updating tests to add test to make sure we can bind correctly to an MVCHash

--- tests/test_listPluginBindToCollection.js
+++ tests/test_listPluginBindToCollection.js
@@ -3,9 +3,9 @@ function testListPluginBindToCollection( Y ) {
 	var Assert = Y.Assert;
 	var TestCase = Y.Test.Case;
 	
-	var test = new TestCase( {
+	var testWithMVCArray = new TestCase( {
 		
-		name: "TestCase AFrame.ListPluginBindToCollection",
+		name: "TestCase AFrame.ListPluginBindToCollection with MVCArray",
 		
 		setUp: function() {
 			this.store = AFrame.construct( {
@@ -68,6 +68,71 @@ function testListPluginBindToCollection( Y ) {
 		}
 	} );
 	
+	var testWithMVCHash = new TestCase( {
+		
+		name: "TestCase AFrame.ListPluginBindToCollection with MVCHash",
+		
+		setUp: function() {
+			this.store = AFrame.construct( {
+				type: 'AFrame.MVCHash'
+			} );
+			
+			this.list = AFrame.construct( {
+				type: 'AFrame.List',
+				config: {
+					target: '#AFrame_List .list',
+					createListElementCallback: function( index, data ) {
+						this.insertedIndex = index;
+						this.insertedData = data;
+						var rowElement = $( '<li id="' + ( data.cid ? data.cid : 'inserted' + index ) + '">Inserted Element</li>' );
+						return rowElement;
+					}.bind( this )
+					
+				},
+				plugins: [
+				{
+					type: 'AFrame.ListPluginBindToCollection',
+					config: {
+						collection: this.store
+					}
+				}
+				]
+			} );
+		},
+		
+		tearDown: function() {
+			this.store.teardown();
+			this.list.clear();
+			this.list.teardown();
+			
+			AFrame.remove( this, 'store' );
+			AFrame.remove( this, 'list' );
+		},
+		
+		testInsert: function() {
+			this.store.insert( { cid: 'inserted0' } );
+			this.store.insert( { cid: 'inserted1' } );
+			
+			Assert.areEqual( 1, $( 'ul > li#inserted0' ).length, 'list element0 inserted' );
+			Assert.areEqual( 1, $( 'ul > li#inserted1' ).length, 'list element1 inserted' );
+		},
+		
+		testRemove: function() {
+			this.store.insert( { cid: 'inserted0' } );
+			this.store.insert( { cid: 'inserted1' } );
+			
+			Assert.areEqual( 1, $( 'ul > li#inserted0' ).length, 'list element0 inserted' );
+			Assert.areEqual( 1, $( 'ul > li#inserted1' ).length, 'list element1 inserted' );
+			
+			this.store.remove( 'inserted0' );
+			Assert.areEqual( 0, $( 'ul > li#inserted0' ).length, 'list element0 removed' );
+			Assert.areEqual( 1, $( 'ul > li#inserted1' ).length, 'list element1 not removed' );
+			
+			this.store.remove( 'inserted1' );
+			Assert.areEqual( 0, $( 'ul > li#inserted1' ).length, 'list element1 removed' );
+		}
+	} );
 	
-	TestRunner.add( test );
+	TestRunner.add( testWithMVCArray );
+	TestRunner.add( testWithMVCHash );
 }
