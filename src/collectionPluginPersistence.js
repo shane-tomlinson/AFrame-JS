e0e9b4b75610975a6853b37e30ff0bbd83a8deb8
Shane Tomlinson <set117@yahoo.com>
2010-10-19 21:12:18 +0000

    Starting to make tests for persistence plugin, starting to add functionality.

--- src/collectionPluginPersistence.js
+++ src/collectionPluginPersistence.js
@@ -10,7 +10,39 @@ AFrame.CollectionPluginPersistence = function() {
 };
 AFrame.extend( AFrame.CollectionPluginPersistence, AFrame.Plugin, {
 	init: function( config ) {
+		/**
+		 * function to call to do add
+		 * @config addCallback
+		 * @type function
+		 */
+		this.addCallback = config.addCallback || this.noPersistenceOp;
+		
+		/**
+		 * function to call to do save
+		 * @config removeCallback
+		 * @type function
+		 */
+		this.saveCallback = config.saveCallback || this.noPersistenceOp;
+		
+		/**
+		 * function to call to do load
+		 * @config loadCallback
+		 * @type function
+		 */
+		this.loadCallback = config.loadCallback || this.noPersistenceOp;
+		
+		/**
+		 * function to call to do delete
+		 * @config deleteCallback
+		 * @type function
+		 */
+		this.deleteCallback = config.deleteCallback || this.noPersistenceOp;
+		
 		AFrame.CollectionPluginPersistence.superclass.init.apply( this, arguments );
+	},
+
+	noPersistenceOp: function( data, meta, callback ) {
+		callback( data, meta );
 	},
 
 	setPlugged: function( plugged ) {
@@ -21,20 +53,51 @@ AFrame.extend( AFrame.CollectionPluginPersistence, AFrame.Plugin, {
 		
 		AFrame.CollectionPluginPersistence.superclass.setPlugged.apply( this, arguments );
 	},
-	
+
+	/**
+	 * Add an item to the collection.
+	 * @method add
+	 * @param {object} data - data to add
+	 * @param {object} meta - meta information.  If callback is supplied in the
+	 * 	meta information, the callback will be called when operation is complete.
+	 */
 	add: function( data, meta ) {
-		this.insert( data, meta );
+		this.addCallback( data, meta, function() {
+			this.getPlugged().insert( data, meta );
+		}.bind( this ) );
 	},
 
+	/**
+	 * load the collection
+	 * @method load
+	 * @param {object} meta - meta information.  If callback is supplied in the
+	 * 	meta information, the callback will be called when operation is complete.
+	 */
 	load: function( meta ) {
 		
 	},
 
-	'delete': function( rowID ) {
-		
+	/**
+	 * delete an item in the collection
+	 * @method delete
+	 * @param {id} itemID - id of item to remove
+	 * @param {object} meta - meta information.  If callback is supplied in the
+	 * 	meta information, the callback will be called when operation is complete.
+	 */
+	'delete': function( itemID , meta ) {
+		this.deleteCallback( itemID, meta, function() {
+			this.getPlugged().remove( itemID, meta );
+		}.bind( this ) );
 	},
 
-	save: function( rowID ) {
+	/**
+	 * save an item in the collection
+	 * @method save
+	 * @param {id} itemID - id of item to save
+	 * @param {object} meta - meta information.  If callback is supplied in the
+	 * 	meta information, the callback will be called when operation is complete.
+	 */
+	save: function( itemID, meta ) {
 		
 	}
 } );
--- tests/test_collectionPluginPersistence.js
+++ tests/test_collectionPluginPersistence.js
@@ -6,7 +6,7 @@ function testCollectionPluginPersistence( Y ) {
 	
 	var test = new TestCase( {
 		
-		name: "TestCase AFrame.CollectionPluginPersistence",
+		name: "TestCase AFrame.CollectionPluginPersistence no callbacks",
 		
 		setUp: function() {
 			this.mockCollection = Y.Mock();
@@ -33,7 +33,37 @@ function testCollectionPluginPersistence( Y ) {
 			Assert.isFunction( this.mockCollection[ 'delete' ], 'delete function added' );
 			Assert.isFunction( this.mockCollection.load, 'load function added' );
 			Assert.isFunction( this.mockCollection.save, 'save function added' );
+		},
+
+		testAdd: function() {
+			Y.Mock.expect( this.mockCollection, {
+				method: 'insert',
+				args: [ Y.Mock.Value.Object, Y.Mock.Value.Object ]
+			} );
+			
+			this.mockCollection.add( {}, {} );
+
+			Y.Mock.verify( this.mockCollection );
+		},
+
+		testDelete: function() {
+			Y.Mock.expect( this.mockCollection, {
+				method: 'insert',
+				args: [ Y.Mock.Value.Object, Y.Mock.Value.Object ]
+			} );
+
+			var rowID = this.mockCollection.add( {}, {} );
+
+			Y.Mock.expect( this.mockCollection, {
+				method: 'remove',
+				args: [ Y.Mock.Value.String, Y.Mock.Value.Object ]
+			} );
+
+			this.mockCollection[ 'delete' ]( 'rowID', {} );
+
+			Y.Mock.verify( this.mockCollection );
 		}
+
 	} );
 
 	TestRunner.add( test );
