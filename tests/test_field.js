e1f7777613f8056f1688a5161f1b88c8522585ce
Shane Tomlinson <set117@yahoo.com>
2010-10-27 19:50:13 +0000

    Adding a textarea test for the field.js

--- tests/test_field.js
+++ tests/test_field.js
@@ -44,6 +44,11 @@ testsToRun.push( function testField( Y ) {
 			this.field.set( 'Preston the Penguin' );
 			Assert.areEqual( 'Preston the Penguin', target.val(), 'element value correctly set when field updated' );
 			Assert.areEqual( 'Preston the Penguin', this.dataContainer.get( 'name' ), 'dataContainer value correctly set when dataContainer updated' );
+
+			target.val( 'beezlebub' );
+			target.trigger( 'change' );
+			Assert.areEqual( 'beezlebub', this.dataContainer.get( 'name' ), 'dataContainer value correctly set value typed into field' );
+			
 		},
 
 		testText: function() {
@@ -65,6 +70,31 @@ testsToRun.push( function testField( Y ) {
 			this.field.set( 'Preston the Penguin' );
 			Assert.areEqual( 'Preston the Penguin', target.html(), 'element value correctly set when field updated' );
 			Assert.areEqual( 'Preston the Penguin', this.dataContainer.get( 'name' ), 'dataContainer value correctly set when dataContainer updated' );
+		},
+
+		testTextArea: function() {
+			var target = $( 'textarea[data-field=name]' );
+			this.field = AFrame.construct( {
+				type: 'AFrame.Field',
+				config: {
+					target: target,
+					dataContainer: this.dataContainer,
+					fieldName: 'name'
+				}
+			} );
+			
+			Assert.areEqual( 'Shane Tomlinson', target.val(), 'input element value correctly set' );
+			
+			this.dataContainer.set( 'name', 'Charlotte Tomlinson' );
+			Assert.areEqual( 'Charlotte Tomlinson', target.val(), 'element value correctly set when dataContainer updated' );
+			
+			this.field.set( 'Preston the Penguin' );
+			Assert.areEqual( 'Preston the Penguin', target.val(), 'element value correctly set when field updated' );
+			Assert.areEqual( 'Preston the Penguin', this.dataContainer.get( 'name' ), 'dataContainer value correctly set when dataContainer updated' );
+
+			target.val( 'beezlebub' );
+			target.trigger( 'change' );
+			Assert.areEqual( 'beezlebub', this.dataContainer.get( 'name' ), 'dataContainer value correctly set value typed into field' );
 		}
 	} );
 
