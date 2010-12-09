f83ad2c5db7e3e64ad4cf49b49b6e1d365a13547
Shane Tomlinson <set117@yahoo.com>
2010-12-09 12:50:02 +0000

    Adding apidocs.js, which takes care of examples.

---
+++ site/scripts/apidocs.js
@@ -0,0 +1,51 @@
+$( function() {
+    var openExample;
+    
+    var showExample = function( event ) {
+        event.preventDefault();
+        event.stopPropagation();
+        
+        openExample && openExample.fadeOut();
+        
+        openExample = $( event.currentTarget ).next( '.example' );
+        openExample.fadeIn();
+    };
+    
+    var hideOpenExample = function() {
+        openExample && openExample.fadeOut();
+        openExample = null;
+    };
+    
+    var hideExample = function( event ) {
+        event.preventDefault();
+        hideOpenExample();
+    };
+    
+    var stopEvent = function( event ) {
+        event.preventDefault();
+        event.stopPropagation();
+    };
+    
+    $( 'body' ).bind( 'click', hideOpenExample ).bind( 'keyup', function( event ) {
+        if( event.which == 27 && openExample ) {
+            hideOpenExample();
+        }
+    } );
+    
+    var examples = $( 'td.description > pre' );
+    examples.each( function( index, item ) {
+        var example = $( item );
+        var anchor = $( '<a href="#" class="btnSeeExample">See Example</a>' );
+        anchor.insertBefore( example );
+        
+        var container = $( '<div class="example"><h3>Example Usage</h3><a href="#" class="btnClose">Close<a></div>' );
+        container.append( example );
+        container.insertAfter( anchor );
+        
+        $( container ).bind( 'click', stopEvent );
+        
+        $( '.btnClose', container ).bind( 'click', hideExample );
+        
+        anchor.bind( 'click', showExample );
+    } );
+} );
