630a93070e774369051bc67a40a42e82fcede84c
Shane Tomlinson <set117@yahoo.com>
2011-01-01 11:36:27 +0000

    adding setValidator to DataValidation.  Changing the name of validators to criteria, and data to dataToValidate to make more sense.

--- src/dataValidation.js
+++ src/dataValidation.js
@@ -1,19 +1,19 @@
 /**
-* Performs data validation, attempts to follow the [HTML5 spec](http://www.whatwg.org/specs/web-apps/current-work/multipage/association-of-controls-and-forms.html#the-constraint-validation-api).
+* Performs dataToValidate validation, attempts to follow the [HTML5 spec](http://www.whatwg.org/specs/web-apps/current-work/multipage/association-of-controls-and-forms.html#the-constraint-validation-api).
 * @class AFrame.DataValidation
 * @static
 */
 AFrame.DataValidation = (function() {
     var defined = AFrame.defined;
     var validationFuncs = {
-        required: function( data, fieldValidityState ) {
-            if( !defined( data ) ) {
+        required: function( dataToValidate, fieldValidityState ) {
+            if( !defined( dataToValidate ) ) {
                 fieldValidityState.setError( 'valueMissing' );
             }
         },
         
-        type: function( data, fieldValidityState, type ) {
-            if( defined( data ) ) {
+        type: function( dataToValidate, fieldValidityState, type ) {
+            if( defined( dataToValidate ) ) {
                 var jsTypes = {
                     text: 'string',
                     number: 'number'
@@ -21,44 +21,44 @@ AFrame.DataValidation = (function() {
                 
                 var jsType = jsTypes[ type ];
                 
-                if( jsType != typeof( data ) ) {
+                if( jsType != typeof( dataToValidate ) ) {
                     fieldValidityState.setError( 'typeMismatch' );
                 }
             }
         
         },
         
-        min: function( data, fieldValidityState, min ) {
-            if( defined( data ) && ( data < min ) ) {
+        min: function( dataToValidate, fieldValidityState, min ) {
+            if( defined( dataToValidate ) && ( dataToValidate < min ) ) {
                 fieldValidityState.setError( 'rangeUnderflow' );
             }
         },
         
-        max: function( data, fieldValidityState, max ) {
-            if( defined( data ) && ( data > max ) ) {
+        max: function( dataToValidate, fieldValidityState, max ) {
+            if( defined( dataToValidate ) && ( dataToValidate > max ) ) {
                 fieldValidityState.setError( 'rangeOverflow' );
             }
         },
         
-        step: function( data, fieldValidityState, step, validators ) {
-            if( defined( data ) ) {
-                var min = validators.min || 0;
-                var valid = 0 === ( ( data - min ) % step );
+        step: function( dataToValidate, fieldValidityState, step, allCriteria ) {
+            if( defined( dataToValidate ) ) {
+                var min = allCriteria.min || 0;
+                var valid = 0 === ( ( dataToValidate - min ) % step );
                 if( !valid ) {
                     fieldValidityState.setError( 'stepMismatch' );
                 }
             }
         },
         
-        maxlength: function( data, fieldValidityState, maxLength ) {
-            if( defined( data ) && data.length && data.length > maxLength ) {
+        maxlength: function( dataToValidate, fieldValidityState, maxLength ) {
+            if( defined( dataToValidate ) && dataToValidate.length && dataToValidate.length > maxLength ) {
                 fieldValidityState.setError( 'tooLong' );
             }
         },
         
-        pattern: function( data, fieldValidityState, pattern ) {
+        pattern: function( dataToValidate, fieldValidityState, pattern ) {
             var regexp = new RegExp( pattern );
-            if( defined( data ) && !regexp.test( data ) ) {
+            if( defined( dataToValidate ) && !regexp.test( dataToValidate ) ) {
                 fieldValidityState.setError( 'patternMismatch' );
             }
         }
@@ -67,7 +67,7 @@ AFrame.DataValidation = (function() {
     var Validation = {
         
         /**
-        * validate the data using the given validators.
+        * validate the dataToValidate using the given validators.
         *
         *    var validators = {
         *        min: 10
@@ -84,23 +84,42 @@ AFrame.DataValidation = (function() {
         *    // fieldValidityState.rangeUnderflow is false
         *
         * @method validate
-        * @param {variant} data - data to validate
+        * @param {variant} dataToValidate - dataToValidate to validate
         * @param {object} validators - validators to use
         * @param {FieldValidityState} fieldValidityState (optional) - 
         *  field validity state to use, one is created if not given
-        * @return {FieldValidityState} [FieldValidityState](AFrame.FieldValidityState.html) for the data.
+        * @return {FieldValidityState} [FieldValidityState](AFrame.FieldValidityState.html) for the dataToValidate.
         */
-        validate: function( data, validators, fieldValidityState ) {
+        validate: function( dataToValidate, criteria, fieldValidityState ) {
             fieldValidityState = fieldValidityState || AFrame.FieldValidityState.getInstance();
             
-            for( var key in validators ) {
+            for( var key in criteria ) {
                 var validator = validationFuncs[ key ];
                 if( validator ) {
-                    validator( data, fieldValidityState, validators[ key ], validators );
+                    validator( dataToValidate, fieldValidityState, criteria[ key ], criteria );
                 }
             }
             
             return fieldValidityState;
+        },
+        
+        /**
+        * Set a validator to be used for a certain type
+        *
+        *    AFrame.DataValidation.addValidator( 'randomtype', function( dataToValidate, fieldValidityState, criteria, otherValidators ) {
+        *       // Do validation here.  If there is a problem, set the error on fieldValidityState
+        *       var valid = // code to do validation
+        *       if( !valid ) {
+        *           fieldValidationState.setCustomValidity( 'invalidRandomType' );
+        *       }
+        *    } );
+        *
+        * @method setValidator
+        * @param {string} type - type to set validator for
+        * @param {function} validator - the validator to use
+        */
+        setValidator: function( type, validator ) {
+            validationFuncs[ type ] = validator;
         }
     };
     
--- tests/test_dataValidation.js
+++ tests/test_dataValidation.js
@@ -9,18 +9,18 @@ testsToRun.push( function testAObject( Y ) {
 		name: "TestCase AFrame.DataValidation",
 		
         testRequired: function() {
-            var validators = {
+            var criteria = {
                 required: true
             };
             
             var fieldValidityState = AFrame.DataValidation.validate( 
-                undefined, validators );
+                undefined, criteria );
             
             Assert.isFalse( fieldValidityState.valid, 'field is invalid' );
             Assert.isTrue( fieldValidityState.valueMissing, 'field is invalid' );
             
             fieldValidityState = AFrame.DataValidation.validate( 
-                validators, 1 );
+                criteria, 1 );
             
             Assert.isTrue( fieldValidityState.valid, 'field is invalid' );
             Assert.isFalse( fieldValidityState.valueMissing, 'field is invalid' );
@@ -28,177 +28,197 @@ testsToRun.push( function testAObject( Y ) {
         },
         
         testMin: function() {
-            var validators = {
+            var criteria = {
                 min: 10
             };
 
             var fieldValidityState = AFrame.DataValidation.validate( 
-                1, validators );
+                1, criteria );
             
             Assert.isFalse( fieldValidityState.valid, 'field is invalid' );
             Assert.isTrue( fieldValidityState.rangeUnderflow, 'value is too low' );
 
             fieldValidityState = AFrame.DataValidation.validate( 
-                10, validators );
+                10, criteria );
             
             Assert.isTrue( fieldValidityState.valid, 'field is valid' );
             Assert.isFalse( fieldValidityState.rangeUnderflow, 'value is good' );
 
             fieldValidityState = AFrame.DataValidation.validate( 
-                100, validators );
+                100, criteria );
             
             Assert.isTrue( fieldValidityState.valid, 'field is valid' );
             Assert.isFalse( fieldValidityState.rangeUnderflow, 'value is good' );
         },
         
         testMax: function() {
-            var validators = {
+            var criteria = {
                 max: 10
             };
 
             var fieldValidityState = AFrame.DataValidation.validate( 
-                100, validators );
+                100, criteria );
             
             Assert.isFalse( fieldValidityState.valid, 'field is invalid' );
             Assert.isTrue( fieldValidityState.rangeOverflow, 'value is too high' );
 
             fieldValidityState = AFrame.DataValidation.validate( 
-                10, validators );
+                10, criteria );
             
             Assert.isTrue( fieldValidityState.valid, 'field is valid' );
             Assert.isFalse( fieldValidityState.rangeOverflow, 'value is good' );
 
             fieldValidityState = AFrame.DataValidation.validate( 
-                1, validators );
+                1, criteria );
             
             Assert.isTrue( fieldValidityState.valid, 'field is valid' );
             Assert.isFalse( fieldValidityState.rangeOverflow, 'value is good' );
         },
         
         testMaxLength: function() {
-            var validators = {
+            var criteria = {
                 maxlength: 10
             };
 
             var fieldValidityState = AFrame.DataValidation.validate( 
-                '', validators );
+                '', criteria );
             
             Assert.isTrue( fieldValidityState.valid, 'field is valid' );
             Assert.isFalse( fieldValidityState.tooLong, 'value is ok length' );
 
             fieldValidityState = AFrame.DataValidation.validate( 
-                '1234567890', validators );
+                '1234567890', criteria );
             
             Assert.isTrue( fieldValidityState.valid, 'field is valid' );
             Assert.isFalse( fieldValidityState.tooLong, 'value is ok length' );
 
             fieldValidityState = AFrame.DataValidation.validate( 
-                '12345678901', validators );
+                '12345678901', criteria );
             
             Assert.isFalse( fieldValidityState.valid, 'field is invalid' );
             Assert.isTrue( fieldValidityState.tooLong, 'value is too long' );
         },
         
         testPattern: function() {
-            var validators = {
+            var criteria = {
                 pattern: '[0-9][A-Z]{3}'
             };
 
             var fieldValidityState = AFrame.DataValidation.validate( 
-                '', validators );
+                '', criteria );
             
             Assert.isFalse( fieldValidityState.valid, 'field is invalid' );
             Assert.isTrue( fieldValidityState.patternMismatch, 'pattern does not match' );
 
             fieldValidityState = AFrame.DataValidation.validate( 
-                '0ABC', validators );
+                '0ABC', criteria );
             
             Assert.isTrue( fieldValidityState.valid, 'field is valid' );
             Assert.isFalse( fieldValidityState.patternMismatch, 'pattern matches' );
         },
         
         testStep: function() {
-            var validators = {
+            var criteria = {
                 step: 2
             };
             
             // step, no min, should use 0 as the min.
             var fieldValidityState = AFrame.DataValidation.validate( 
-                .33, validators );
+                .33, criteria );
             
             Assert.isFalse( fieldValidityState.valid, 'field is invalid' );
             Assert.isTrue( fieldValidityState.stepMismatch, '.33 is an invalid step' );
 
             // step, no min, should use 0 as the min.
             fieldValidityState = AFrame.DataValidation.validate( 
-                2, validators );
+                2, criteria );
             
             Assert.isTrue( fieldValidityState.valid, 'field is valid' );
             Assert.isFalse( fieldValidityState.stepMismatch, '2 is a valid step' );
             
             // Add a non-standard min to see if it works.
-            validators.min = 3;
+            criteria.min = 3;
 
             fieldValidityState = AFrame.DataValidation.validate( 
-                3, validators );
+                3, criteria );
             
             Assert.isTrue( fieldValidityState.valid, '3 is valid' );
             Assert.isFalse( fieldValidityState.stepMismatch, '3 is a valid step when .33 is min' );
 
             // step, with min
             fieldValidityState = AFrame.DataValidation.validate( 
-                5, validators );
+                5, criteria );
 
             Assert.isTrue( fieldValidityState.valid, '5 is valid' );
             Assert.isFalse( fieldValidityState.stepMismatch, '5 is a valid step when .33 is min' );
 
             // step, with min
             fieldValidityState = AFrame.DataValidation.validate( 
-                4, validators );
+                4, criteria );
 
             Assert.isFalse( fieldValidityState.valid, '4 is an invalid step' );
             Assert.isTrue( fieldValidityState.stepMismatch, '4 is an invalid step when 3 is min' );
         },
         
         testType: function() {
-            var validators = {
+            var criteria = {
                 type: 'text'
             };
 
             var fieldValidityState = AFrame.DataValidation.validate( 
-                1, validators );
+                1, criteria );
             
             Assert.isFalse( fieldValidityState.valid, 'text field is invalid' );
             Assert.isTrue( fieldValidityState.typeMismatch, '1 is not text' );
 
             
             var fieldValidityState = AFrame.DataValidation.validate( 
-                'asdf', validators );
+                'asdf', criteria );
             
             Assert.isTrue( fieldValidityState.valid, 'text field is valid' );
             Assert.isFalse( fieldValidityState.typeMismatch, 'asdf is text' );
 
-            var validators = {
+            var criteria = {
                 type: 'number'
             };
 
             var fieldValidityState = AFrame.DataValidation.validate( 
-                1, validators );
+                1, criteria );
             
             Assert.isTrue( fieldValidityState.valid, 'number field is valid' );
             Assert.isFalse( fieldValidityState.typeMismatch, '1 is a number' );
 
             var fieldValidityState = AFrame.DataValidation.validate( 
-                'asdf', validators );
+                'asdf', criteria );
             
             Assert.isFalse( fieldValidityState.valid, 'number field is invalid' );
             Assert.isTrue( fieldValidityState.typeMismatch, 'asdf is not a number' );
 
             var fieldValidityState = AFrame.DataValidation.validate( 
-                '1', validators );
+                '1', criteria );
             
             Assert.isFalse( fieldValidityState.valid, 'number field is invalid' );
             Assert.isTrue( fieldValidityState.typeMismatch, "'1' is not a number" );
+        },
+        
+        testSetCustomValidator: function() {
+            var validator = function( data, fieldValidityState, criteria, allCriteria ) {
+                fieldValidityState.setCustomValidity( 'randomValidationError' );
+            };
+            
+            AFrame.DataValidation.setValidator( 'randomtype', validator );
+            
+            var criteria = {
+                randomtype: 'criteria'
+            };
+            
+            var fieldValidityState = AFrame.DataValidation.validate( 
+                1, criteria );
+                
+            
+            Assert.isFalse( fieldValidityState.valid, 'randomValidationError caused invalid field' );
+            Assert.isTrue( fieldValidityState.customError, 'randomValidationError caused customError' );
+            Assert.areEqual( 'randomValidationError', fieldValidityState.validationMessage, 'randomValidationError set for validationMessage' );
         }
 	
 	} );
