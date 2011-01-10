49712fbf68c586e1fad057bb9c48e18b3575ac40
Shane Tomlinson <set117@yahoo.com>
2011-01-10 23:42:47 +0000

    Starting to add AFrame.Event.

---
+++ src/event.js
@@ -0,0 +1,132 @@
+/**
+* A basic event, very loosly modeled after [W3C DOM Level 2 Events](http://www.w3.org/TR/DOM-Level-2-Events/events.html)
+* @class AFrame.Event
+* @constructor
+*/
+/**
+* The event type
+* @config type
+* @type {string}
+*/
+/**
+* When the event was created.
+* @property timestamp
+* @type {Date}
+*/
+/**
+* Type of event.
+* @property type
+* @type {string}
+*/
+/**
+* event target, not always set
+* @property target
+* @type {object}
+*/
+/**
+* original event target, before any proxying, not always set
+* @property originalTarget
+* @type {object}
+*/
+AFrame.Event = (function() {
+    var Event = function() {};
+    Event.prototype = {
+        constructor: Event,
+        /**
+        * initialize the event.  All items in configuration will be added to event.  If timestamp
+        *   is specified, it will be ignored.  type must be specified.
+        * @param {object} config
+        * @param {string} config.type - type of event
+        */
+        init: function( config ) {
+            for( var key in config ) {
+                this[ key ] = config[ key ];
+            }
+            
+            if( !this.type ) {
+                throw 'Error type undefined';
+            }
+            
+            if( this.target ) {
+                this.setOriginalTarget = true;
+            }
+            
+            this.timestamp = new Date();
+        },
+        
+        /**
+        * Check if preventDefault has been called.
+        *
+        *     // Check if preventDefault has been called
+        *     var isPrevented = event.isDefaultPrevented();
+        *
+        * @method isDefaultPrevented
+        * @return {booelan} true if preventDefault has been called, false otw.
+        */
+        isDefaultPrevented: function() {
+            return !!this.defaultPrevented;
+        },
+        
+        /**
+        * Cancel the default action of the event.  Note, this does nothing on its own,
+        *   any object that passes an Event object must check isDefaultPrevented to see
+        *   whether an action should be cancelled.
+        *
+        *    // Prevent the default action
+        *    event.preventDefault();
+        *
+        * @method preventDefault
+        */
+        preventDefault: function() {
+            this.defaultPrevented = true;
+        },
+        
+        /**
+        * Proxy an event.  If this is the first time the event is proxied, causes
+        *   originalTarget to be set to the original target, and updates target to
+        *   point to the proxy.
+        *
+        *    event.proxyEvent( proxy );
+        *
+        * @method proxyEvent
+        * @param {object} proxy - object proxying event
+        */
+        proxyEvent: function( proxy ) {
+            if( this.setOriginalTarget ) {
+                this.originalTarget = this.target;
+                this.setOriginalTarget = false;
+            }
+            
+            this.target = proxy;
+        }
+    };
+    
+    /**
+    * A factory method to create an event.
+    *
+    *    // returns an event with event.type == 'eventType'
+    *    var event = Event.createEvent( 'eventType' );
+    *
+    *    // returns an event with event.type == 'eventType', extraField == 'extraValue'
+    *    var event = Event.createEvent( {
+    *        type: 'eventType',
+    *        extraField: 'extraValue'
+    *    } );
+    *
+    * @method Event.createEvent
+    * @param {object||string} config - if an object, object is used as Event config, 
+    *   if a string, the string signifies the type of event
+    * @return {AFrame.Event} event with type
+    */
+    Event.createEvent = function( config ) {
+        var event = AFrame.construct( {
+            type: AFrame.Event,
+            config: 'string' == typeof( config ) ? {
+                type: config
+            } : config
+        } );
+        return event;
+    };
+    
+    return Event;
+})();
---
+++ tests/test_event.js
@@ -0,0 +1,116 @@
+testsToRun.push( function testDisplay( Y ) {
+	var TestRunner = Y.Test.Runner;
+	var Assert = Y.Assert;
+	var TestCase = Y.Test.Case;
+	
+	var test = new TestCase( {
+		
+		name: "TestCase AFrame.Event",
+		
+		setUp: function() {
+            this.event = AFrame.Event.createEvent( 'event' );
+		},
+		
+		tearDown: function () {
+            this.event = null;
+		},
+        
+        testEvent: function() {
+            var event = this.event;
+            
+            Assert.areEqual( 'event', event.type, 'event.type set correctly to event' );
+            Assert.isTrue( event.timestamp instanceof Date, 'event.timestamp is a date' );
+        },
+        
+        testPreventDefault: function() {
+            var event = this.event;
+            
+            Assert.isFalse( event.isDefaultPrevented(), 'default has not yet been prevented' );
+            
+            event.preventDefault();
+            Assert.isTrue( event.isDefaultPrevented(), 'default has been prevented' );
+        },
+        
+        testCreateEventWithObject: function() {
+            var event = AFrame.Event.createEvent( {
+                type: 'event',
+                target: this,
+                extraField: 'extraValue'
+            } );
+            
+            Assert.areEqual( 'event', event.type, 'type correctly set' );
+            Assert.areEqual( this, event.target, 'target correctly set' );
+            Assert.areEqual( 'extraValue', event.extraField, 'extraField correctly set' );
+            Assert.isTrue( event.timestamp instanceof Date, 'timestamp set' );
+        },
+        
+        testEventNoType: function() {
+            var error;
+            
+            try {
+                var event = AFrame.Event.createEvent( {
+                    target: this,
+                    extraField: 'extraValue'
+                } );
+            } catch ( e ) {
+                error = e;
+            }
+            
+            Assert.areEqual( 'Error type undefined', error, 'no type error was thrown for createEvent with object' );
+
+            error = undefined;
+            
+            try {
+                var event = AFrame.Event.createEvent();
+            } catch ( e ) {
+                error = e;
+            }
+            
+            Assert.areEqual( 'Error type undefined', error, 'no type error was thrown for createEvent with no arguments' );
+
+            error = undefined;
+            
+            try {
+                var event = AFrame.Event.createEvent( '' );
+            } catch ( e ) {
+                error = e;
+            }
+            
+            Assert.areEqual( 'Error type undefined', error, 'no type error was thrown for createEvent with empty string' );
+        },
+        
+        testProxyEventNoTarget: function() {
+            var proxy = {};
+            this.event.proxyEvent( proxy );
+            
+            Assert.areEqual( proxy, this.event.target, 'event successfully proxied' );
+            Assert.isUndefined( this.event.originalTarget, 'originalTarget undefined' );
+            
+            var secondProxy = {};
+            this.event.proxyEvent( secondProxy );
+            Assert.areEqual( secondProxy, this.event.target, 'event successfully proxied to second proxy' );
+            Assert.isUndefined( this.event.originalTarget, 'originalTarget undefined' );
+        },
+        
+        testProxyEventTarget: function() {
+            var event = AFrame.Event.createEvent( {
+                target: this,
+                type: 'event'
+            } );
+            
+            var proxy = {};
+            event.proxyEvent( proxy );
+            Assert.areEqual( proxy, event.target, 'event successfully proxied' );
+            Assert.areEqual( this, event.originalTarget, 'originalTarget set to this' );
+
+            var secondProxy = {};
+            event.proxyEvent( secondProxy );
+            Assert.areEqual( secondProxy, event.target, 'event successfully proxied to second proxy' );
+            Assert.areEqual( this, event.originalTarget, 'originalTarget still set to this' );
+        }
+
+
+	} );
+
+	TestRunner.add( test );
+} );
