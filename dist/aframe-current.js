86367e7b19fa9a133b3bd1663cd24ca015f4aa29
Shane Tomlinson <set117@yahoo.com>
2011-01-10 15:08:47 +0000

    Making Display's teardown call the superclass's.  Making Display use the Module pattern.

--- dist/aframe-current.js
+++ dist/aframe-current.js
@@ -1642,184 +1642,191 @@ AFrame.CollectionArray = (function(){
  * @extends AFrame.AObject
  * @constructor
  */
-AFrame.Display = function() {
-	AFrame.Display.sc.constructor.apply( this, arguments );
-};
-AFrame.Display.currDOMEventID = 0;
-AFrame.extend( AFrame.Display, AFrame.AObject, {
-	/**
-	 * the target
-	 * @config target
-	 * @type {element || selector}
-	 */
-	init: function( config ) {
-		this.target = $( config.target );
-		
-		if( !this.target.length ) {
-			throw 'invalid target';
-		}
+AFrame.Display = (function() {
+    var currDOMEventID = 0;
 
-        this.render();
-        
-		this.domEvents = {};
-		
-		AFrame.Display.sc.init.apply( this, arguments );
-	},
-
-	teardown: function() {
-		for( var key in this.domEvents ) {
-			this.unbindDOMEvent( key );
-		}
-
-		this.target = null;
-	},
-	
-    
-    /**
-    * Render the HTML element, by default, only triggers the onRender observable.  Should be overridden in 
-    *   subclasses to do any templating, setting up the DOM, etc.  Subclasses do not need to do anything
-    *   if the full DOM for this display has already been created.  AFrame does not care what templating system
-    *   that is used, so any way of setting the target's HTML is fine.
-    *
-    *
-    *     ...
-    * 
-    *     // Example of render which directly inserts HTML
-    *     render: function() {
-    *         this.getTarget().html( '<div>This is rendered inside of ' +
-    *              'the Dislay\'s target</div>' );
-    *     },
-    *
-    *     // Example of using jTemplate to render a template
-    *     render: function() {
-    *         this.getTarget().setTemplate( $( '#template' ).html() ).processTemplate( {} );
-    *     },
-    * 
-    *
-    * @method render
-    */
-    render: function() {
+    var Display = function() {
+        Display.sc.constructor.apply( this, arguments );
+    };
+    AFrame.extend( Display, AFrame.AObject, {
         /**
-        * Triggered whenever the displayed is rendered.
-        * @event onRender
-        * @param {AFrame.Display} display - the display being rendered.
+         * the target
+         * @config target
+         * @type {element || selector}
+         */
+        init: function( config ) {
+            this.target = $( config.target );
+            
+            if( !this.target.length ) {
+                throw 'invalid target';
+            }
+
+            this.render();
+            
+            this.domEvents = {};
+            
+            Display.sc.init.call( this, config );
+        },
+
+        teardown: function() {
+            for( var key in this.domEvents ) {
+                this.unbindDOMEvent( key );
+            }
+
+            this.target = null;
+            
+            Display.sc.teardown.call( this );
+        },
+        
+        
+        /**
+        * Render the HTML element, by default, only triggers the onRender observable.  Should be overridden in 
+        *   subclasses to do any templating, setting up the DOM, etc.  Subclasses do not need to do anything
+        *   if the full DOM for this display has already been created.  AFrame does not care what templating system
+        *   that is used, so any way of setting the target's HTML is fine.
+        *
+        *
+        *     ...
+        * 
+        *     // Example of render which directly inserts HTML
+        *     render: function() {
+        *         this.getTarget().html( '<div>This is rendered inside of ' +
+        *              'the Dislay\'s target</div>' );
+        *     },
+        *
+        *     // Example of using jTemplate to render a template
+        *     render: function() {
+        *         this.getTarget().setTemplate( $( '#template' ).html() ).processTemplate( {} );
+        *     },
+        * 
+        *
+        * @method render
         */
-        this.triggerEvent( 'onRender', this );
-    },
+        render: function() {
+            /**
+            * Triggered whenever the displayed is rendered.
+            * @event onRender
+            * @param {Display} display - the display being rendered.
+            */
+            this.triggerEvent( 'onRender', this );
+        },
+        
+        /**
+         * Get the display's target.
+         *
+         *    var target = display.getTarget();
+         *
+         * @method getTarget
+         * @return {element} target
+         */
+        getTarget: function() {
+            return this.target;
+        },
+        
+        /**
+        * Get the display's native DOM Element.
+        * 
+        *    var element = display.getDOMElement();
+        *
+        * @method getDOMElement
+        * @return {HTMLElement} - the DOM Element
+        */
+        getDOMElement: function() {
+            return this.target[ 0 ];
+        },
+
+        /**
+         * Bind to a DOM event
+         *
+         *    var onClose = function( event ) {
+         *      // close something here
+         *    };
+         *    var id = display.bindDOMEvent( '.btnClose', 'click', onClose );
+         *    // use id to unbind DOM event
+         *
+         * @method bindDOMEvent
+         * @param {element || selector} target - the target.  If a string, searches the DOM
+         * @param {string} eventName - the name of the event to bind to
+         * @param {function} callback - the callback to callback
+         * @param {object} context (optional)- the context to call the callback in, if not given, use this.
+         * @return {id} id that can be used to unbind the event.
+         */
+        bindDOMEvent: function( target, eventName, callback, context ) {
+            var eventCallback = callback.bind( context || this );
+            var eventTarget = this.getEventTarget( target );
+            eventTarget.bind( eventName, eventCallback );
+
+            currDOMEventID++;
+            var id = currDOMEventID;
+            this.domEvents[ id ] = {
+                target: eventTarget,
+                eventName: eventName,
+                callback: eventCallback
+            };
+            
+            return id;
+        },
+        
+        /**
+        * a convenience function for binding click events.  The event has it's default prevented so that
+        *	if binding to an anchor with an href of "#", the screen does not jump.
+        *
+        *    var onClose = function( event ) {
+        *      // event's default is already prevented
+        *      // close something here
+        *    };
+        *    var id = display.bindClick( '.btnClose', onClose );
+        *    // use id to unbind DOM event
+        *
+        * @method bindClick
+        * @param {element || selector} target - the target.  If a string, searches the DOM
+        * @param {function} callback - the callback to callback
+        * @param {object} context (optional)- the context to call the callback in, if not given, use this.
+        * @return {id} id that can be used to unbind the event.
+        */
+        bindClick: function( target, callback, context ) {
+            return this.bindDOMEvent( target, 'click', function( event ) {
+                event.preventDefault();
+                callback.call( context, event );
+            }, context );
+        },
+        
+        /**
+         * Unbind a DOM event
+         *
+         *    var id = display.bindDOMEvent( ... );
+         *    display.unbindDOMEvent( id );
+         *
+         * @method unbindDOMEvent
+         * @param {id} id - id of event to unbind
+         */
+        unbindDOMEvent: function( id ) {
+            var event = this.domEvents[ id ];
+            if( event ) {
+                event.target.unbind( event.eventName, event.callback );
+                event.target = null;
+                event.eventName = null;
+                event.callback = null;
+                AFrame.remove( this.domEvents, id );
+            }
+        },
+
+        getEventTarget: function( target ) {
+            var eventTarget;
+
+            if( 'string' == typeof( target ) ) {
+                eventTarget = $( target, this.getTarget() );
+            }
+            else {
+                eventTarget = $( target );
+            }
+            
+            return eventTarget;
+        }
+    } );
     
-	/**
-	 * Get the display's target.
-     *
-     *    var target = display.getTarget();
-     *
-	 * @method getTarget
-	 * @return {element} target
-	 */
-	getTarget: function() {
-		return this.target;
-	},
-	
-	/**
-	* Get the display's native DOM Element.
-    * 
-    *    var element = display.getDOMElement();
-    *
-	* @method getDOMElement
-	* @return {HTMLElement} - the DOM Element
-	*/
-	getDOMElement: function() {
-		return this.target[ 0 ];
-	},
-
-	/**
-	 * Bind to a DOM event
-     *
-     *    var onClose = function( event ) {
-     *      // close something here
-     *    };
-     *    var id = display.bindDOMEvent( '.btnClose', 'click', onClose );
-     *    // use id to unbind DOM event
-     *
-	 * @method bindDOMEvent
-	 * @param {element || selector} target - the target.  If a string, searches the DOM
-	 * @param {string} eventName - the name of the event to bind to
-	 * @param {function} callback - the callback to callback
-	 * @param {object} context (optional)- the context to call the callback in, if not given, use this.
-	 * @return {id} id that can be used to unbind the event.
-	 */
-	bindDOMEvent: function( target, eventName, callback, context ) {
-		var eventCallback = callback.bind( context || this );
-		var eventTarget = this.getEventTarget( target );
-		eventTarget.bind( eventName, eventCallback );
-
-		AFrame.Display.currDOMEventID++;
-		var id = AFrame.Display.currDOMEventID;
-		this.domEvents[ id ] = {
-			target: eventTarget,
-			eventName: eventName,
-			callback: eventCallback
-		};
-		
-		return id;
-	},
-	
-	/**
- 	* a convenience function for binding click events.  The event has it's default prevented so that
-	*	if binding to an anchor with an href of "#", the screen does not jump.
-    *
-    *    var onClose = function( event ) {
-    *      // event's default is already prevented
-    *      // close something here
-    *    };
-    *    var id = display.bindClick( '.btnClose', onClose );
-    *    // use id to unbind DOM event
-    *
-	* @method bindClick
-	* @param {element || selector} target - the target.  If a string, searches the DOM
-	* @param {function} callback - the callback to callback
-	* @param {object} context (optional)- the context to call the callback in, if not given, use this.
-	* @return {id} id that can be used to unbind the event.
-	*/
-	bindClick: function( target, callback, context ) {
-		return this.bindDOMEvent( target, 'click', function( event ) {
-			event.preventDefault();
-			callback.call( context, event );
-		}, context );
-	},
-	
-	/**
-	 * Unbind a DOM event
-     *
-     *    var id = display.bindDOMEvent( ... );
-     *    display.unbindDOMEvent( id );
-     *
-	 * @method unbindDOMEvent
-	 * @param {id} id - id of event to unbind
-	 */
-	unbindDOMEvent: function( id ) {
-		var event = this.domEvents[ id ];
-		if( event ) {
-			event.target.unbind( event.eventName, event.callback );
-			event.target = null;
-			event.eventName = null;
-			event.callback = null;
-			AFrame.remove( this.domEvents, id );
-		}
-	},
-
-	getEventTarget: function( target ) {
-		var eventTarget;
-
-		if( 'string' == typeof( target ) ) {
-			eventTarget = $( target, this.getTarget() );
-		}
-		else {
-			eventTarget = $( target );
-		}
-		
-		return eventTarget;
-	}
-} );
+    return Display;
+} )();
 /**
  * A generic HTML list class.  A list is any list of data.  A List shares
  *  the majority of its interface with a <a href="docs/AFrame.CollectionArray.html">CollectionArray</a> 
