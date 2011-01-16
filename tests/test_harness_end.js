050e3e7b5d92bd6d2edf384c6e13528753068e19
Shane Tomlinson <set117@yahoo.com>
2011-01-16 16:45:01 +0000

    Using cookies/JSON in the test so that when we reload the test page, the last tests run are the only ones run on reload.

--- tests/test_harness_end.js
+++ tests/test_harness_end.js
@@ -8,9 +8,8 @@ YUI( { logInclude: { TestRunner: true } } ).use( 'console', 'overlay', 'test', f
     testsToRun.forEach( function( testConfig, index ) {
     	var test = new TestCase( testConfig );
         testCases.push( test );
-        TestRunner.add( test );
         
-        $( '<li><input type="checkbox" value="' + index + '" checked />' + test.name + '</li>' ).appendTo( $( '#testList' ) );
+        $( '<li><input type="checkbox" value="' + index + '" />' + test.name + '</li>' ).appendTo( $( '#testList' ) );
     } );
 
     //initialize the console
@@ -19,44 +18,80 @@ YUI( { logInclude: { TestRunner: true } } ).use( 'console', 'overlay', 'test', f
     } );
     yconsole.render( '#log' );
 
-
-
-    TestRunner.run();
+    var items = [];
+    var cookie = $.cookie( 'tests_to_run' );
+    if( cookie ) {
+        items = JSON.parse( cookie ) || [];
+    }
+    else {
+        items.push( 'all' );
+    }
     
+    items.forEach( function( itemID, index ) {
+        $( '#testList input[type=checkbox][value=' + itemID + ']' ).attr( 'checked', true );
+    } );
+    runItems( items );
     
     $( '#btnRunTests' ).click( function() {
-        TestRunner.clear();
-        
-        $( 'input[type=checkbox]:checked' ).each( function( index, item ) {
-            var index = ~~$( item ).val();
-            
-            TestRunner.add( testCases[ index ] );
-        } );
-        
-        TestRunner.run();
+        var items = getChecked();
+        runItems( items );
     } );
     
     $( '#chkAllTests' ).click( function( event ) {
         var checked = $( this ).is( ':checked' );
         
         if( checked ) {
-            $( 'input[type=checkbox]' ).attr( 'checked', true );
+            $( '#testList input[type=checkbox]' ).attr( 'checked', true );
         }
         else {
-            $( 'input[type=checkbox]' ).removeAttr( 'checked' );
+            $( '#testList input[type=checkbox]' ).removeAttr( 'checked' );
         }
     } );
     
-    $( 'input[type=checkbox]' ).click( function( event ) {
+    $( '#testList input[type=checkbox]' ).click( function( event ) {
         var el = $( this );
         if( false == el.is( '#chkAllTests' ) ) {
             var checked = el.is( ':checked' );
             if( false == checked ) {
                 $( '#chkAllTests' ).removeAttr( 'checked' );
             }
-            
         }
     } );
 
+    
+    $( window ).bind( 'beforeunload', function( event ) {
+        var items = getChecked();
+        var cookie = JSON.stringify( items );
+        $.cookie( 'tests_to_run', cookie );
+    } );
+
+    function getChecked() {
+        var checkedIndex = [];
+
+        $( '#testList input[type=checkbox]:checked' ).each( function( index, item ) {
+            var testID = $( item ).val();
+            
+            checkedIndex.push( testID );
+        } );
+        
+        return checkedIndex;
+    }
+
+    function runItems( items ) {
+        TestRunner.clear();
+        
+        if( items.indexOf( 'all' ) > -1 ) {
+            testCases.forEach( function( test ) {
+                TestRunner.add( test );
+            } );
+        }
+        else {
+            items.forEach( function( testID, index ) {
+                TestRunner.add( testCases[ testID ] );
+            } );
+        }
+
+        TestRunner.run();
+    }
 } );
 
