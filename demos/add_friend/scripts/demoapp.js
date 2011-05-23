76331896d4d28328dedb65e598e61b87854bab54
Shane Tomlinson <set117@yahoo.com>
2011-05-23 17:25:13 +0100

    updating for new way of creating objects

--- demos/add_friend/scripts/demoapp.js
+++ demos/add_friend/scripts/demoapp.js
@@ -1,40 +1,43 @@
-$( function() {
-    
-    // Instead of creating a model, create a SchemaConfig.  A Model in AFrame is an instance of
-    //  data combined with a Schema.  The schema config will be used when creating the model.
+$(function() {
+    // Instead of creating a model, create a SchemaConfig.
+    // A Model in AFrame is an instance of data combined
+    // with a Schema.  The schema config will be used when
+    // creating the model.
     var friendSchemaConfig = {
         name: { type: 'text' }
     };
 
-    // Our friends collection is an array.  Instead of binding directly to an event of the
-    //  collection, when creating the list, we bind the list to the collection.  
-    //  The ListPluginBindToCollection will take care of the updating of the list.
-    var friendsCollection = AFrame.create( AFrame.CollectionArray, {
+    // Our friends collection is an array.  Instead of binding
+    // directly to an event on the collection, when creating
+    // the list, we bind the list to the collection.
+    // The ListPluginBindToCollection will take care of
+    // updating of the list.
+    var friendsCollection = AFrame.CollectionArray.create( {
         plugins: [ [ AFrame.CollectionPluginModel, {
-                schema: friendSchemaConfig
-            }
-        ] ]
+             schema: friendSchemaConfig
+        } ] ]
     } );
-    
-    // This is the friends list.  It is bound to the friendsCollection, so any time a model
-    //  is added or removed from the friends collection, the listElementFactory will update
-    //  the list.
-    var friendsList = AFrame.create( AFrame.List, {
+
+    // This is the friends list.  It is bound to the
+    // friendsCollection, so any time a model is added or
+    //  removed from the friends collection, the list will update.
+    var friendsList = AFrame.List.create( {
         target: '#friendList',
         listElementFactory: function( model, index ) {
-            return AFrame.DOM.createElement( 'li', model.get( 'name' ) );
+            return AFrame.DOM.createElement( 'li',
+                model.get( 'name' ) );
         },
         plugins: [ [ AFrame.ListPluginBindToCollection, {
-                collection: friendsCollection
-            }
-        ] ]
+             collection: friendsCollection
+        } ] ]
     } );
-    
-    // we insert into the friendsCollection once we have a name.  The list will be 
-    //  automatically updated.
+
+    // we insert into the friendsCollection once we
+    // have a name.  The list will be updated automatically.
     $( '#add-friend' ).click( function( event ) {
-           var friend_name = prompt( "Who is your friend?" );
-           // Add new model will be created when adding to the friend collection.
+           var friend_name = prompt("Who is your friend?");
+           // A new model will be created when adding
+           // to the friend collection.
            friendsCollection.insert( { name: friend_name } );
     } );
-} );
+});
